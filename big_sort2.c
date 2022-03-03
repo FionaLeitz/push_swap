ce56682c78dbd3ae2c98062c6badaf3b935a373e
Fiona LEITZ <fleitz@e1r5p15.42.fr>
2022-03-03 16:22:54 +0100

    cest bon !!!!!

--- big_sort2.c
+++ big_sort2.c
@@ -6,7 +6,7 @@
 /*   By: fleitz <fleitz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 09:43:14 by fleitz            #+#    #+#             */
-/*   Updated: 2022/03/01 17:55:27 by fleitz           ###   ########.fr       */
+/*   Updated: 2022/03/03 16:06:50 by fleitz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,17 @@
 
 void	ft_fonctiona(t_stack **stck_a, t_stack **stck_b, t_args *info, int *rp)
 {
-	while ((--rp[0] > 0 || rp[1] > 0) && info->newsize-- >= 0/* && (*stck_b)*/)
+	if (info->newsize < rp[0] - 1)
+	{
+		while (info->newsize-- > 0)
+		{
+			info->str = ft_string(stck_a, stck_b, info->str, "pa\n");
+			if ((*stck_a)->next != NULL)
+				info->str = ft_string(stck_a, stck_b, info->str, "ra\n");
+		}
+		return ;
+	}
+	while ((--rp[0] > 0 || rp[1] > 0) && info->newsize-- >= 0)
 	{
 		if ((*stck_b) && ((*stck_a)->nbr > (*stck_b)->nbr
 			|| rp[1] - rp[0] == 0) && rp[1]-- > 0)
@@ -23,8 +33,34 @@ void	ft_fonctiona(t_stack **stck_a, t_stack **stck_b, t_args *info, int *rp)
 	}
 }
 
+int	ft_count(t_stack **stack)
+{
+	int	count;
+
+	count = 1;
+	if (stack == NULL)
+		return (0);
+	while ((*stack)->next)
+	{
+		(*stack) = (*stack)->next;
+		count++;
+	}
+	ft_back_one(stack);
+	return (count);
+}
+
 void	ft_fonctionb(t_stack **stck_a, t_stack **stck_b, t_args *info, int *rp)
 {
+	if (info->newsize < rp[0] - 1)
+	{
+		while (info->newsize-- > 0)
+		{
+			info->str = ft_string(stck_a, stck_b, info->str, "pb\n");
+			if ((*stck_b)->next != NULL)
+				info->str = ft_string(stck_a, stck_b, info->str, "rb\n");
+		}
+		return ;
+	}
 	while ((--rp[0] > 0 || rp[1] > 0) && info->newsize-- >= 0 && (*stck_b))
 	{
 		if (((*stck_b)->nbr > (*stck_a)->nbr || rp[1] - rp[0] == 0)
@@ -54,12 +90,27 @@ void	ft_fonction3(t_stack **stack_a, t_stack **stack_b, t_args *info, int *s)
 	}
 }
 
-int	ft_fonction4(t_stack **stack_b, int repere, t_args *info, int *save)
+int	ft_fonction4(t_stack **stack_a, t_stack **stack_b, t_args *info, int *save)
 {
-	(void)stack_b;
-	if (info->newsize < repere/* && (*stack_b)*/)
+//	(void)stack_b;
+//	(void)stack_a;
+
+	if (info->newsize <= save[2])
 	{
+//		ft_printf("ICI !\nnewsize = %d\nsave[2] = %d\n", info->newsize, save[2]);
+//		ft_printf("stack_b = %p\n", (*stack))
 		save[0] = 2;
+		if (*stack_b == NULL)
+		{
+			while (info->newsize > 0)
+			{
+//				ft_printf("ICI \n");
+				info->str = ft_string(stack_a, stack_b, info->str, "pb\n");
+				if ((*stack_b)->next != NULL)
+					info->str = ft_string(stack_a, stack_b, info->str, "rb\n");
+				info->newsize--;
+			}
+		}
 		return (1);
 	}
 	return (0);
@@ -73,8 +124,9 @@ void	ft_big_lists(t_stack **stack_a, t_stack **stack_b, t_args *info)
 	save[2] = 8;
 	info->newsize = info->size;
 	save[0] = 1;
-	while (info->newsize >= save[2]/* && (*stack_b)*/)
+	while (info->newsize >= (save[2]) && (*stack_b))
 	{
+//		ft_printf("Debut : newsize = %d\nsave[2] = %d\n", info->newsize, save[2]);
 //		ft_printf("Debut du big while :\n");
 //		ft_print(stack_a, stack_b);
 		save[1] = (*stack_b)->nbr;
@@ -82,30 +134,30 @@ void	ft_big_lists(t_stack **stack_a, t_stack **stack_b, t_args *info)
 			save[1] = (*stack_a)->nbr;
 		while (info->newsize >= save[2] && (*stack_b))
 		{
-//			ft_printf("newsize = %d\nsave[2] = %d\n", info->newsize, save[2]);
-
-///////////////////////
-//	reparer pour 17, casser pour tout le reste
-			if (save[2] * 2 >= info->newsize)
-			{
-				ft_memcpy(&rp, (int [2]){save[2] + 1, save[2] / 2}, 8);
-				ft_fonctionb(stack_a, stack_b, info, rp);
-				return ;
-			}
-///////////////////////
-
+//			ft_printf("Debut du little while :\n");
+//			ft_print(stack_a, stack_b);
 			ft_memcpy(&rp, (int [2]){save[2] + 1, save[2] / 2}, 8);
 			ft_fonctiona(stack_a, stack_b, info, rp);
-			if (ft_fonction4(stack_b, save[2], info, save) == 1)
+//			ft_printf("Milieu du little while :\n");
+//			ft_print(stack_a, stack_b);
+			if (ft_fonction4(stack_a, stack_b, info, save) == 1)
 			{
+//				ft_printf("Fin du little while special :\n");
+//				ft_print(stack_a, stack_b);
 				break ;
 			}
 			ft_memcpy(&rp, (int [2]){save[2] + 1, save[2] / 2}, 8);
+//			ft_printf("rp[0] = %d\nrp[1] = %d\nsave[1] = %d\nnewsize = %d\n", rp[0], rp[1], save[1], info->newsize);
 			ft_fonctionb(stack_a, stack_b, info, rp);
+//			ft_printf("Fin du little while :\n");
+//			ft_print(stack_a, stack_b);
 		}
 		ft_fonction3(stack_a, stack_b, info, save);
 		save[2] = save[2] * 2;
 		info->newsize = info->size;
+//		ft_printf("Fin du big while :\n");
+//		ft_print(stack_a, stack_b);
+//		ft_printf("Fin : newsize = %d\nsave[2] = %d\n", info->newsize, save[2]);
 	}
 	return ;
 }
